182120393eb9354bbf71a3ef937a67673c9707f1
bdanglot <bdanglot@gmail.com>
2016-09-26 13:30:55 +0200

    feat: precondition supported and undo avalaible after applying the patch

--- src/plugin/wrapper/ApplyPatchWrapper.java
+++ src/plugin/wrapper/ApplyPatchWrapper.java
@@ -1,17 +1,21 @@
 package plugin.wrapper;
 
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.actionSystem.DocCommandGroupId;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.ValidationInfo;
 import com.intellij.psi.*;
+import com.intellij.psi.codeStyle.CodeStyleManager;
 import com.intellij.psi.search.EverythingGlobalScope;
 import fr.inria.lille.repair.common.patch.Patch;
+import fr.inria.lille.repair.common.synth.StatementType;
 import fr.inria.lille.repair.nopol.SourceLocation;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -20,8 +24,6 @@ import plugin.gui.ApplyPatchPanel;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.util.List;
-
-import static com.intellij.testFramework.LightPlatformTestCase.getProject;
 
 /**
  * Created by bdanglot on 9/21/16.
@@ -34,7 +36,7 @@ public class ApplyPatchWrapper extends DialogWrapper {
 
 	private Patch selectedPatch;
 	private PsiStatement patchStatement;
-	private PsiExpression buggyExpression;
+	private PsiElement buggyElement;
 
 	public ApplyPatchWrapper(Project project, List<Patch> patches) {
 		super(true);
@@ -86,14 +88,12 @@ public class ApplyPatchWrapper extends DialogWrapper {
 		PsiClass classToBeFix = JavaPsiFacade.getInstance(this.project).findClass(this.selectedPatch.getRootClassName(), new EverythingGlobalScope(this.project));
 		classToBeFix.accept(new JavaRecursiveElementVisitor() {
 			@Override
-			public void visitIfStatement(PsiIfStatement statement) {
-				//TODO check the computation of position
+			public void visitStatement(PsiStatement statement) {
 				if (location.getBeginSource() == statement.getTextOffset() &&
 						location.getEndSource() == statement.getTextOffset() + statement.getTextLength() - 1) {
-					buggyExpression = statement.getCondition();
-					System.out.println("This conditional " + statement.getCondition().getText() + " should replaced by " + selectedPatch.asString());
+					buggyElement = statement;
 				}
-				super.visitIfStatement(statement);
+				super.visitStatement(statement);
 			}
 		});
 	}
@@ -106,19 +106,44 @@ public class ApplyPatchWrapper extends DialogWrapper {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			CommandProcessor.getInstance().executeCommand(getProject(), () -> ApplicationManager.getApplication().runWriteAction( () -> {
-				PsiClass classToBeFix = JavaPsiFacade.getInstance(project).findClass(selectedPatch.getRootClassName(), new EverythingGlobalScope(project));
-				OpenFileDescriptor descriptor = new OpenFileDescriptor(project, classToBeFix.getContainingFile().getVirtualFile(), buggyExpression.getTextOffset());
-				Editor editor = FileEditorManager.getInstance(project).openTextEditor(descriptor, true);
-				Document modifiedDocument = editor.getDocument();
+			PsiClass classToBeFix = JavaPsiFacade.getInstance(project).findClass(selectedPatch.getRootClassName(), new EverythingGlobalScope(project));
+			OpenFileDescriptor descriptor = new OpenFileDescriptor(project, classToBeFix.getContainingFile().getVirtualFile(), buggyElement.getTextOffset());
+			Editor editor = FileEditorManager.getInstance(project).openTextEditor(descriptor, true);
+			Document modifiedDocument = editor.getDocument();
+
+			final String patch;
+
+			if (selectedPatch.getType() == StatementType.CONDITIONAL) {
+				buggyElement = ((PsiIfStatement) buggyElement).getCondition();
+				patch = patchStatement.getText();
+			} else {
+				String newline = FileDocumentManager.getInstance().getFile(modifiedDocument).getDetectedLineSeparator();
+				StringBuilder sb = new StringBuilder();
+				sb.append("if( ");
+				sb.append(patchStatement.getText());
+				sb.append(" ) {" + newline);
+				sb.append(buggyElement.getText() + newline);
+				sb.append("}");
+				patch = sb.toString();
+			}
+
+			CommandProcessor.getInstance().executeCommand(project, () -> WriteCommandAction.runWriteCommandAction(project, () -> {
 				//Apply the patch
-				modifiedDocument.replaceString(buggyExpression.getTextOffset(), buggyExpression.getTextOffset() + buggyExpression.getTextLength(), patchStatement.getText());
+				modifiedDocument.replaceString(buggyElement.getTextOffset(), buggyElement.getTextOffset() + buggyElement.getTextLength(), patch);
 				//Move caret to modification
-				editor.getCaretModel().moveToOffset(buggyExpression.getTextOffset());
+				editor.getCaretModel().moveToOffset(buggyElement.getTextOffset());
 				//Select patch
-				editor.getSelectionModel().setSelection(buggyExpression.getTextOffset(), buggyExpression.getTextOffset() + buggyExpression.getTextLength());
-				close(0);
-			}), "ApplyPatch", CommandProcessor.getInstance().getCurrentCommandGroupId());
+				editor.getSelectionModel().setSelection(buggyElement.getTextOffset(), buggyElement.getTextOffset() +
+						(selectedPatch.getType() == StatementType.CONDITIONAL ? buggyElement.getTextLength() : patch.length()));
+				PsiDocumentManager.getInstance(project).commitDocument(modifiedDocument);
+				CodeStyleManager.getInstance(project).reformat(PsiDocumentManager.getInstance(project).getPsiFile(modifiedDocument), false);
+			}), "ApplyPatch", DocCommandGroupId.noneGroupId(modifiedDocument));
+
+			PsiDocumentManager.getInstance(project).commitDocument(modifiedDocument);
+
+			close(0);
 		}
 	}
+
+
 }
