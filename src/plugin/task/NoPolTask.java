3e532f6314392aa829bcdec3f985e9b949d0aeac
bdanglot <bdanglot@gmail.com>
2016-09-26 15:01:39 +0200

    add robot while Nopol is searching a patch

--- src/plugin/task/NoPolTask.java
+++ src/plugin/task/NoPolTask.java
@@ -1,23 +1,26 @@
 package plugin.task;
 
-import fr.inria.lille.repair.actor.ConfigActor;
-import fr.inria.lille.repair.common.patch.Patch;
-import fr.inria.lille.repair.nopol.NoFailingTestCaseException;
-import fr.inria.lille.repair.nopol.NoSuspiciousStatementException;
-import plugin.actors.ActorManager;
 import akka.pattern.Patterns;
 import akka.util.Timeout;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import fr.inria.lille.repair.actor.ConfigActor;
+import fr.inria.lille.repair.common.patch.Patch;
+import fr.inria.lille.repair.nopol.NoFailingTestCaseException;
+import fr.inria.lille.repair.nopol.NoSuspiciousStatementException;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import plugin.Plugin;
+import plugin.actors.ActorManager;
 import plugin.wrapper.ApplyPatchWrapper;
 import scala.concurrent.Await;
 import scala.concurrent.Future;
 
+import javax.swing.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -39,12 +42,27 @@ public class NoPolTask extends Task.Backgroundable {
 	private Object response;
 	private Future<Object> future;
 
+	private JFrame frame;
+
+	private final Runnable runnerFancyRobot = () -> {
+		this.frame = new JFrame();
+		JLabel imageLabel = new JLabel();
+		imageLabel.setIcon(new ImageIcon(this.getClass().getResource("/giphy.gif")));
+		this.frame.getContentPane().add(imageLabel);
+		this.frame.setVisible(true);
+		this.frame.setLocationRelativeTo(null);
+		this.frame.pack();
+	};
+
 	@Override
 	public void run(@NotNull ProgressIndicator progressIndicator) {
 		Timeout timeout = new Timeout(200000);
 		try {
 			ConfigActor configActor = new ConfigActor(config, Files.readAllBytes(Paths.get(outputZip)));
 			this.future = Patterns.ask(ActorManager.remoteActor, configActor, timeout);
+			if (Plugin.enableFancyRobot) {
+				ApplicationManager.getApplication().invokeLater(runnerFancyRobot);
+			}
 			this.response = Await.result(future, timeout.duration());
 		} catch (Exception e) {
 			onError(e);
@@ -53,13 +71,18 @@ public class NoPolTask extends Task.Backgroundable {
 
 	@Override
 	public void onError(@NotNull Exception error) {
+		if (Plugin.enableFancyRobot)
+			this.frame.dispose();
 		Messages.showMessageDialog(getProject(), error.getMessage(), "Error", Messages.getErrorIcon());
 	}
 
 	@Override
 	public void onSuccess() {
 		super.onSuccess();
-		//TODO maybe add super class
+
+		if (Plugin.enableFancyRobot)
+			this.frame.dispose();
+
 		if (this.response instanceof NoSuspiciousStatementException) {
 			Messages.showMessageDialog(getProject(), this.response.toString(), ((NoSuspiciousStatementException) this.response).header, Messages.getWarningIcon());
 		} else if (this.response instanceof NoFailingTestCaseException) {
@@ -80,6 +103,8 @@ public class NoPolTask extends Task.Backgroundable {
 	public void onCancel() {
 		super.onCancel();
 		this.future.failed();
+		if (Plugin.enableFancyRobot)
+			this.frame.dispose();
 		Messages.showMessageDialog(getProject(), "The job has been cancelled", "Cancelled", Messages.getErrorIcon());
 	}
 
